304768d379bcfa0ddecb962c51e642570d8b0646
Bielikov Maksym <maximax6767@gmail.com>
2025-06-27 21:24:03 +0300

    Refactor Icecast stream proxy iter_any function

--- app/api/routes/stream.py
+++ app/api/routes/stream.py
@@ -7,6 +7,8 @@ from fastapi import Request, APIRouter
 from app.bot.player.streamer import ffmpeg_streamer
 
 MAX_STREAM_RETRIES = 5
+ICECAST_URL = "http://localhost:8001/stream"
+HEADERS = {"User-Agent": "FastAPI-Proxy"}
 
 logger = logging.getLogger(__name__)
 stream_router = APIRouter(prefix="/stream", tags=["Stream webhook"])
@@ -14,17 +16,14 @@ stream_router = APIRouter(prefix="/stream", tags=["Stream webhook"])
 
 @stream_router.get("")
 async def proxy_icecast_stream(request: Request):
-    icecast_url = "http://localhost:8001/stream"
-    headers = {"User-Agent": "FastAPI-Proxy"}
-
     try:
         async with ClientSession(timeout=ClientTimeout(total=None)) as session:
-            resp = await session.get(icecast_url, headers=headers)
+            resp = await session.get(ICECAST_URL, headers=HEADERS)
             if resp.status != 200:
                 await ffmpeg_streamer.start()
                 await asyncio.sleep(3)
 
-                resp = await session.get(icecast_url, headers=headers)
+                resp = await session.get(ICECAST_URL, headers=HEADERS)
                 if resp.status != 200:
                     return JSONResponse(
                         status_code=resp.status,
@@ -36,15 +35,13 @@ async def proxy_icecast_stream(request: Request):
                 try:
                     while True:
                         try:
-                            chunk = await asyncio.wait_for(
-                                resp.content.readany(), timeout=15
-                            )
-                            yield chunk
-                            retries = 0
-                        except asyncio.TimeoutError:
+                            async for chunk in resp.content.iter_any():
+                                yield chunk
+                                retries = 0
+                        except Exception:
                             retries += 1
                             logger.warning(
-                                f"Timeout while reading stream. Retry {retries}/{MAX_STREAM_RETRIES}"
+                                f"Exception while reading stream. Retry {retries}/{MAX_STREAM_RETRIES}"
                             )
                             if retries >= MAX_STREAM_RETRIES:
                                 logger.error("Max retries reached. Ending stream.")
