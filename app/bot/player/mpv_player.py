f8e07632c637184eb9eeb5c6c00bfe2ce3a03679
Bielikov Maksym <maximax6767@gmail.com>
2026-03-24 12:09:59 +0200

    Temporary remove cookies

--- app/bot/player/mpv_player.py
+++ app/bot/player/mpv_player.py
@@ -16,8 +16,8 @@ from app.settings import settings
 
 ytdl_opts = "extractor-args=youtube:player_client=android"
 
-if settings.COOKIES_PATH:
-    ytdl_opts += f",cookies={settings.COOKIES_PATH}"
+# if settings.COOKIES_PATH:
+#     ytdl_opts += f",cookies={settings.COOKIES_PATH}"
 
 
 class MPVPlayer(mpv.MPV):
